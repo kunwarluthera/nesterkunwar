664ba4f0df29986ce50890c580921e7d30f80f9b
Kunwar Luthera <kunwarluthera@Kunwars-MBP.frontierlocal.net>
2019-02-03 16:33:48 -0600

    initial commit

--- nester.py
+++ nester.py
@@ -1,10 +1,11 @@
-def print_lol(the_list, level=0):  # Addition of the DEFUALT value for level has made it optional
+def print_lol(the_list,indent=False,level=0):  # Addition of the DEFUALT value for level has made it optional
     for each_item in the_list:
         if isinstance(each_item,list):
             print_lol(each_item,level+1)
         else:
-            for tabs in range(level):
-                print("\t", end="")
+            if indent:
+                for tabs in range(level):
+                    print("\t", end="")
             print(each_item)
 
 
--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ def read(fname):
 
 setup(
 
-    name = "nester-kunwar",
-    version = "1.1.1",
-    py_modules = ["nester-kunwar"],
+    name = "nesterkunwar",
+    version = "1.0.0",
+    py_modules = ["nesterkunwar"],
     author = "Kunwar Shamsher Luthera",
     author_email = "kunwarluthera@gmail.com",
     url = "https://github.com/kunwarluthera/nester-kunwar",
